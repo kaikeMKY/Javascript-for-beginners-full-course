b7c99d81facd603c0870b7807f033ce53e69ec0a
kaikeMKY <kaikemassaokamidayamashita@gmail.com>
2025-03-22 18:45:24 -0300

    deny

--- Stringvar.js
+++ Stringvar.js
@@ -1,20 +1,24 @@
-var myFumo ="cirno"; // declare the string variable
-var myYukkuri ="marisa";// declare the string variable
+var myFumo = "cirno"; // declare the string variable
+
+var myYukkuri = "marisa"; // declare the string variable
+
 //double quotes
-var reimu ="I AM\"NOT\"BAKA" //OR
-reimu = 'baka"baka"baka'
+var reimu = 'I AM"NOT"BAKA'; //OR
+reimu = 'baka"baka"baka';
+
 var phase = "i am";
-phase +="Baka";
+phase += "Baka"; //then will be "i am baka"
+
 //22
-var MyName = "Baka"
-var phase2 = "im not"+ MyName +" ok?";
-//var my fumo = cirno 
-var adjetivo = "Funky"
-adjetivo += myFumo;// Funky cirno :D
+var MyName = "Baka";
+var phase2 = "im not" + MyName + " ok?"; //im not baka ok?
+
+//var my fumo = cirno
+var adjetivo = "Funky";
+adjetivo += myFumo; // Funky cirno :D
+
 var letra1;
-letra1 = myFumo[0];//C
+letra1 = myFumo[0]; //C of cirno start in 0 instend of 1
+
 var ultimaLetra;
-ultimaLetra = myFumo[myFumo.length-1]//o pega tamanho da palavra e -1 é a ultima letra
-
-
-
+ultimaLetra = myFumo[myFumo.length - 1]; //o pega tamanho da palavra e -1 é a ultima letra
--- how2Run.js
+++ how2Run.js
@@ -2,9 +2,6 @@
 console.log("hello Baka");
 print("Baka");
 
-//this is a comment just last on this line 
+//this is a comment just last on this line
 /* this is also a comment but now can last long 
 as the * / be in */
-
-
-
