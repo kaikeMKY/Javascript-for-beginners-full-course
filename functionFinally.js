c78face1cf7b8a5dd3761d7cbbf66b8b01d060af
kaikeMKY <kaikemassaokamidayamashita@gmail.com>
2025-03-23 16:58:01 -0300

    ok i thinks is now 70% of the course

--- functionFinally.js
+++ functionFinally.js
@@ -455,3 +455,112 @@ function phoneticLookup(val){
   return result;
 }
 console.log(phoneticLookup("alpha"));
+//
+var myObj= {
+  gift: "pony",
+  pet: "dogo",
+  bed: "sleigh"
+};
+function checkobj (checkprop){
+  if (myObj.hasOwnPropety(checkprop)){
+    return myObj[checkprop];
+  }else {
+    return "not Found"
+  }
+}
+console.log(checkobj("gift"));
+//
+var myMusic=[
+  {
+    "artist": "A-one",
+    "title": "temptSign",
+    "formats": [ "digital","mp3"
+    ],
+    "tohoEuro": "true"
+  },
+]
+//
+var Mystorage = {
+  "car": {
+    "inside": {
+      "glovebox": "maps",
+      "passagerseat": "crumbs"//blud dont clean his car
+    },
+    "outside": {
+      "trunk":"jack"
+    }
+  }
+};
+var gloveboxcontents= Mystorage.car.inside["glovebox"];
+
+console.log(gloveboxcontents)
+
+//
+var myplants = [
+  {
+    type:"myPlants",
+    list: [
+      "weed",
+      "zaza",
+      "bonsai"
+    ]
+  },
+  {
+  type: "momPlants",
+  list: [ "none"//LOL
+    ]
+  }
+];
+var mymomplants = myplants[1].list[0];
+//
+var collection ={
+  "1919": {
+    "album": "",
+    "artist": "",
+    "tracks": [
+      "",
+      ""
+    ]
+  },
+  "2222": {
+    "album": "",
+    "artist": "littleV",
+    "tracks": [
+      "",
+      ""
+    ]
+  },
+  "0001": {
+    "album": "",
+    "artist": "",
+    "tracks": [
+      "",
+      ""
+    ]
+  },
+};
+var collectioncopy= JSON.parse(JSON.stringify(collection));
+
+function updateRecords(id,prop,value) {
+  if (value ===""){
+    delete collection[id][prop];
+  }else if (prop=== "tracks"){
+    collection[id][prop] =collection[id][prop] || [];
+    collection[id][prop].push(value);
+  }else {
+    collection[id][prop]=value;
+  }
+
+  return collection;
+}
+updateRecords( 1919,"tracks","test");
+console.log(updateRecords(2222,"artist",littleV));
+//
+var myArray = [];
+var i =0;
+while(i<5) {
+  myArray.push(i);
+  i++;
+}
+console.log(myArray);
+//
